809b10855281c5305e010fc8fab8444e4452b487
ayanamist <ayanamist@gmail.com>
2013-04-26 15:22:03 +0800

    Complete Cleaner. Refactor.

--- WeiboTrainer.user.js
+++ WeiboTrainer.user.js
@@ -7,6 +7,9 @@
 // @match http://account.weibo.com/*
 // @version 1.0
 // @run-at document-start
+// @grant GM_listValues
+// @grant GM_getValue
+// @grant GM_setValue
 // ==/UserScript==
 
 (function (window) {
@@ -18,6 +21,23 @@
     var Utils = {
         isReady: function () {
             return /interactive|complete/i.test(document.readyState);
+        },
+        _deferQueue: [],
+        init: function() {
+            var finishQueue = function () {
+                    document.removeEventListener("DOMContentLoaded", listener);
+                    Array.prototype.forEach.call(Utils._deferQueue, function (func) {
+                        func();
+                    });
+                },
+                listener = document.addEventListener("DOMContentLoaded", finishQueue, false);
+
+            if (Utils.isReady()) {
+                finishQueue();
+            }
+        },
+        deferReady: function(func) {
+            Utils._deferQueue.push(func);
         },
         makeRandomStr: function (length) {
             var possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
@@ -36,6 +56,16 @@
                 node = nodes[i];
                 node.parentNode.removeChild(node);
             }
+        },
+        addNamedStyle: function(css, name) {
+            var style = document.getElementById(name);
+            if (!style) {
+                style = document.createElement("style");
+                style.type = "text/css";
+                style.id = name;
+                (document.head||document.documentElement).appendChild(style);
+            }
+            style.innerHTML = css;
         }
     };
 
@@ -67,7 +97,6 @@
     var ObjProxy = {
         MSG_NAME: "wbtr_" + Utils.makeRandomStr(8),
         _callbackNumPool: new NamePool("wbtrcb_"),
-        _readyQueue: [],
         _delegateScript: function (msgName) {
             document.addEventListener(msgName, function (evt) {
                 var objName = evt.detail.name,
@@ -152,19 +181,8 @@
             var script = document.createElement("script");
             script.innerHTML = "(" + ObjProxy._delegateScript.toString() + ")('" + ObjProxy.MSG_NAME + "');";
             script.type = "text/javascript";
-            document.getElementsByTagName("head")[0].appendChild(script);
+            (document.head||document.documentElement).appendChild(script);
             script = null;
-            var finishQueue = function () {
-                    document.removeEventListener("DOMContentLoaded", listener);
-                    Array.prototype.forEach.call(ObjProxy._readyQueue, function (job) {
-                        ObjProxy._remoteExecute.apply(null, job);
-                    });
-                },
-                listener = document.addEventListener("DOMContentLoaded", finishQueue, false);
-
-            if (Utils.isReady()) {
-                finishQueue();
-            }
         },
         getByName: function (objName, callback) {
             ObjProxy._remoteExecute({
@@ -182,28 +200,110 @@
         }
     };
     var Cleaner = {
-        maybeInterested: function () {
+        0: function () {
             // 可能感兴趣的人
             return "#trustPagelet_recom_interestv5 {display: none !important;}";
         },
-        layerTips: function () {
+        1: function () {
             // 浮动提示
             return ".layer_tips_intro {display: none !important;}";
         },
-        bizTips: function () {
+        2: function () {
             // 时间线上面横幅广告
-            ObjProxy.getByName("$CONFIG.uid", function (uid) {
-                if (typeof uid !== "undefined") {
-                    ObjProxy.callByName("STK.core.util.cookie.set", ["tips_" + uid, "1", {"expire": 36500}]);
-                }
+            Utils.deferReady(function(){
+                ObjProxy.getByName("$CONFIG.uid", function (uid) {
+                    if (typeof uid !== "undefined") {
+                        ObjProxy.callByName("STK.core.util.cookie.set", ["tips_" + uid, "1", {"expire": 36500}]);
+                    }
+                });
             });
             return "#pl_content_biztips {display: none !important;}";
         },
-        adActivity: function () {
+        3: function () {
+            // 右侧边 [活动]
             return "#pl_rightmod_ads35 {display: none !important;}";
+        },
+        4: function () {
+            // 右侧边 时间线底部横幅广告
+            return "div[ad-data*=ads_bottom] {display: none !important;}";
+        },
+        5: function () {
+            // 右侧边 热门话题、我的话题
+            return "#trustPagelet_zt_hottopicv5 {display: none !important;}";
+        },
+        6: function () {
+            // 右侧边 会员专区、会员动态
+            return "#trustPagelet_recom_memberv5 {display: none !important;}";
+        },
+        7: function () {
+            // 右侧边 微吧、微刊、应用
+            return "#trustPagelet_recom_allinonev5 {display: none !important;}";
+        },
+        8: function () {
+            // 右侧边 热门商品推荐
+            return "#pl_rightmod_ads36 {display: none !important;}";
+        },
+        9: function () {
+            // 右侧边 公告栏
+            return "#pl_rightmod_noticeboard {display: none !important;}";
+        },
+        10: function () {
+            // 左侧边 最近使用
+            return "#pl_leftnav_app {display: none !important;}";
+        },
+        11: function () {
+            // 时间线上面 热门微博
+            return "div[node-type=recommendTopic] {display: none !important;}";
+        },
+        12: function () {
+            // 时间线底部 开通会员提示
+            return "div[node-type=feed_list_shieldKeyword] {display: none !important;}";
+        },
+        13: function () {
+            // 用户页面 微关系
+            return "#pl_profile_moduleHisRelation {display: none !important;}";
+        },
+        14: function () {
+            // 用户页面 微相册
+            return "#pl_profile_modulealbum {display: none !important;}";
+        },
+        15: function () {
+            // 用户页面 应用卡片（微刊等）
+            return "#trustPagelet_profile_openApplist {display: none !important;}";
+        },
+        16: function () {
+            // 陌生用户页面 加关注提示
+            return "#pl_profile_unfollow {display: none !important;}";
+        },
+        17: function () {
+            // 用户页面 封面图
+            // 注意把勋章移动到关注按钮上面
+            Utils.deferReady(function(){
+                var pf_tags = document.querySelector("#pl_profile_hisInfo .pf_tags"),
+                    pf_badge_icon = document.querySelector("#pl_profile_cover .pf_badge_icon");
+                if (pf_tags && pf_badge_icon) {
+                    pf_tags.appendChild(pf_badge_icon);
+                }
+            });
+            return ".profile_top .pf_head{top: 5px !important;}\
+                  #pl_profile_cover {display: none !important;} \
+                  #plc_profile_header .profile_top {min-height: 234px} \
+                  #pl_profile_hisInfo .pf_badge_icon li {display: inline-block; margin-top: 5px;}";
+        },
+        18: function () {
+            // 用户页面 勋章
+            return ".pf_badge_icon {display: none !important;}";
         }
     };
 
     ObjProxy.init();
-    Cleaner.bizTips();
+    Utils.init();
+
+    var cssList = [],
+        styleName = Utils.makeRandomStr(8);
+    Array.prototype.forEach.call(Object.keys(Cleaner), function(k) {
+        cssList.push(Cleaner[k]());
+    });
+    Utils.addNamedStyle(cssList.join(""), styleName);
+
 })(window);
